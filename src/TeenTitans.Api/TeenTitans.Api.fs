7f672e10af6a8ba7ebc4fc0996d7b3c5d5bb33d9
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-24 01:51:59 -0500

    wire up routes

--- src/TeenTitans.Api/TeenTitans.Api.fs
+++ src/TeenTitans.Api/TeenTitans.Api.fs
@@ -1,9 +1,27 @@
-module TeenTitansApi.Main
+namespace TeenTitansApi
 
-open Suave
-open Suave.Successful
+module App =   
+    open Suave.Web
+    open SuaveRestApi.Rest
+    open SuaveRestApi.Db
+    open SuaveRestApi.MusicStoreDb
+    open Suave
 
-[<EntryPoint>]
-let main argv =
-    startWebServer defaultConfig (OK "Hello World!")
-    0
+    [<EntryPoint>]
+    let main argv =    
+
+        let titansWebPart = rest "titan" {
+            GetAllTitans = Db.getTitan
+            GetByTitanId = Db.getTitan
+            CreateTitan = Db.createTitan
+            UpdateTitan = Db.updateTitan
+            UpdateTitanById = Db.updateTitanById
+            DeleteTitan = Db.deleteTitan
+            TitanExists = Db.isTitanExists
+        }
+
+	let app = choose[titanWebPart]
+
+        startWebServer defaultConfig app
+            
+        0
