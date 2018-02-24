745506f9ece1bb10cfb15b86024ae6a5e6d3ffb0
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-24 01:37:48 -0500

    add Get routes

--- src/TeenTitans.Api/Titans.fs
+++ src/TeenTitans.Api/Titans.fs
@@ -19,6 +19,31 @@ module Titans =
         req.rawForm |> getString |> fromJson<'a>
 
 
-  type TitansResource<'a> = {
-    GetAllTitans : unit -> 'a seq        
-  }
+    type TitansResource<'a> = {
+        GetAllTitans : unit -> 'a seq        
+        GetTitanById : int -> 'a option        
+	TitanExists : int -> 'a bool        
+    }
+
+    let rest resourceName resource =
+
+        let resourcePath = "/" + resourceName
+        let resourceIdPath = new PrintfFormat<(int -> string),unit,string,string,int>(resourcePath + "/%d")
+        let badRequest = BAD_REQUEST "Resource not found"
+        let handleResource requestError = function
+            | Some r -> r |> JSON
+            | _ -> requestError
+
+        let getAllResources= warbler (fun _ -> resource.GetAll () |> JSON)
+        let getResourceById = 
+            resource.GetTitanById >> handleResource (NOT_FOUND "Titan not found")
+        let ResourceExists id =
+            if resource.TitanExists id then OK "" else NOT_FOUND ""
+
+        choose [
+            path resourcePath >=> choose [
+                GET >=> getAllResources
+            ]
+            GET >=> pathScan resourceIdPath getResourceById
+            HEAD >=> pathScan resourceIdPath ResourceExists
+        ]
