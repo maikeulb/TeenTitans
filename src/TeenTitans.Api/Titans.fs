57fd8286b7e2828a3278b30809b89b6c764440a4
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-24 01:25:36 -0500

    Add Json serializer

--- src/TeenTitans.Api/Titans.fs
+++ src/TeenTitans.Api/Titans.fs
@@ -3,6 +3,22 @@ namespace TeenTitansApi.Rest
 [<AutoOpen>]
 module Titans =  
 
+    let JSON v =     
+        let jsonSerializerSettings = new JsonSerializerSettings()
+        jsonSerializerSettings.ContractResolver <- new CamelCasePropertyNamesContractResolver()
+    
+        JsonConvert.SerializeObject(v, jsonSerializerSettings)
+	|> OK 
+	>=> Writers.setMimeType "application/json; charset=utf-8"
+
+    let fromJson<'a> json =
+        JsonConvert.DeserializeObject(json, typeof<'a>) :?> 'a    
+
+    let getResourceFromReq<'a> (req : HttpRequest) = 
+        let getString rawForm = System.Text.Encoding.UTF8.GetString(rawForm)
+        req.rawForm |> getString |> fromJson<'a>
+
+
   type TitansResource<'a> = {
     GetAllTitans : unit -> 'a seq        
   }
