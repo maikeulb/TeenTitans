6bcce9469f851b43aae8a07c99e618ef8646da79
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-24 01:46:46 -0500

    Add Db

---
+++ src/TeenTitans.Api/Db.fs
@@ -0,0 +1,43 @@
+namespace TeenTitansApi.Db
+
+open System.Collections.Generic
+
+type Titan = {
+    Id : int
+    Name : string
+    Age : int
+    Email : string
+}
+
+module Db =
+
+    let titanStorage = new Dictionary<int, Titan>()
+    let getTitan () =
+        titanStorage.Values :> seq<Titan>
+    let getTitan id =
+        if titanStorage.ContainsKey(id) then
+            Some titanStorage.[id]
+        else
+            None
+    let createTitan person =
+        let id = titanStorage.Values.Count + 1
+        let newTitan = {person with Id = id}
+        titanStorage.Add(id, newTitan)
+        newTitan
+
+    let updateTitanById personId personToBeUpdated =
+        if titanStorage.ContainsKey(personId) then
+            let updatedTitan = {personToBeUpdated with Id = personId}
+            titanStorage.[personId] <- updatedTitan
+            Some updatedTitan
+        else
+            None
+
+    let updateTitan personToBeUpdated =
+        updateTitanById personToBeUpdated.Id personToBeUpdated
+
+    let deleteTitan personId =
+        titanStorage.Remove(personId) |> ignore
+
+    let TitanExists  = titanStorage.ContainsKey
+
